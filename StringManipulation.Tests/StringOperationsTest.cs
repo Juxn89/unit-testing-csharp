5ffcac747d91ee4171107d6c70088fbab380288a
Juan Gómez <gomezj01@stepstone.com>
2024-03-06 08:57:16 -0600

    Primera prueba unitaria con xUnit

---
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,13 @@
+﻿namespace StringManipulation.Tests
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+            var result = strOperation.ConcatenateStrings("Hello", "world");
+            Assert.Equal("Hello world", result);
+        }
+    }
+}
