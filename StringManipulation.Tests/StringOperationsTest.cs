696f87767f5fb44f237e24f65f956340a183f06a
Juan Gómez <gomezj01@stepstone.com>
2024-03-06 10:21:14 -0600

    Tipos de Assert part. 2

--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -41,5 +41,53 @@
             // Assert
             Assert.False(result);
         }
+
+        [Fact]
+        public void RemoveWhitespace() 
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.RemoveWhitespace("This sentence has white spaces");
+
+            // Assert
+            var template = "Thissentencehaswhitespaces";
+            Assert.Equal(template, result);
+            Assert.DoesNotContain(" ", result);
+        }
+
+        [Fact]
+        public void QuantintyInWords()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.QuantintyInWords("cat", 10);
+
+            // Assert
+            Assert.StartsWith("ten", result);
+            Assert.Contains("cat", result);
+            Assert.Equal("ten cats", result);
+        }
+
+        [Fact]
+        public void GetStringLength()
+        {
+            var strOperation = new StringOperations();
+
+            var result = strOperation.GetStringLength("MS .NET");
+
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void GetStringLength_Exception()
+        {
+            var strOperation = new StringOperations();
+
+            Assert.ThrowsAny<ArgumentNullException>( () => strOperation.GetStringLength(null));
+        }
     }
 }
