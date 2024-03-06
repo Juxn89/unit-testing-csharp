684003ee4521bd43c2304bd3ea0b0120fb24e8ff
Juan Gómez <gomezj01@stepstone.com>
2024-03-06 09:48:22 -0600

    Tipos de Assert con xUnit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# unit-testing-csharp
-Unit testing con C# y .NET
+# Unit Testing con C# y .NET
+## Lecturas
+- [F.I.R.S.T princples for testing](https://medium.com/@tasdikrahman/f-i-r-s-t-principles-of-testing-1a497acda8d6)
--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -5,9 +5,41 @@
         [Fact]
         public void ConcatenateStrings()
         {
+            // Arrange
             var strOperation = new StringOperations();
+
+            // Act
             var result = strOperation.ConcatenateStrings("Hello", "world");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello world", result);
+        }
+
+        [Fact]
+        public void IsPalindrome_True() { 
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.IsPalindrome("ama");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange
+            var strOperation = new StringOperations();
+
+            // Act
+            var result = strOperation.IsPalindrome("hello");
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
